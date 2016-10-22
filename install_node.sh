a67f6d7de391a34f3a4f4c636218a392993b4d2c
Tomo Masakura <fj0706kr@aa.jp.fujitsu.com>
2016-10-22 14:07:42 +0900

    change from gulp to gulp-cli

--- install_node.sh
+++ install_node.sh
@@ -5,6 +5,6 @@ NODE_VERSION=$1
 . $NVM_DIR/nvm.sh
 
 nvm install $NODE_VERSION
-nvm exec $NODE_VERSION npm install -g grunt-cli gulp bower
+nvm exec $NODE_VERSION npm install -g grunt-cli gulp-cli bower
 nvm alias default $NODE_VERSION
 
